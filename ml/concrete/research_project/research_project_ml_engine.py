7c2e637d597e42741f8b3976099ce923ed6305f4
Jasiek2r <jasiek2r@gmail.com>
2026-03-23 13:36:13 +0100

    fix: move model to gpu if possible

--- ml/concrete/research_project/research_project_ml_engine.py
+++ ml/concrete/research_project/research_project_ml_engine.py
@@ -16,7 +16,7 @@ class ResearchProjectMLEngine(MLEngine):
     def __init__(self, device=None):
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
         #self.model = ResearchProjectNet().to(self.device)
-        self.model = self.create_model()
+        self.model = self.create_model().to(self.device)
 
     def train(self, train_dataset, val_dataset, batch_size=8, epochs=50):
 
