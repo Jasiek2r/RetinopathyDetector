0a0ae3479c6caa228ac49a13522151a9b97632ce
Jasiek2r <jasiek2r@gmail.com>
2026-03-29 11:11:36 +0200

    chore: change dataset path

--- startup.py
+++ startup.py
@@ -12,7 +12,7 @@ class Startup:
     def __build_application__(self) -> RetinopathyApplication:
         # configure constants here
         sample_limit = None  # use None for no limit
-        path = "./aptos2019-blindness-detection"
+        path = "./research-project-dataset"
         dev = True  # uses developer version for testing
 
         # configure the dependencies used by application here
