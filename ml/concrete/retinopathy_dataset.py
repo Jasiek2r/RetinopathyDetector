4cbb8da51e02fe762d8cf5f04da5c15ad9bd416f
Jasiek2r <jasiek2r@gmail.com>
2026-04-29 16:21:25 +0200

    fix: remove balanced subset per class parameter

--- ml/concrete/retinopathy_dataset.py
+++ ml/concrete/retinopathy_dataset.py
@@ -18,7 +18,6 @@ class RetinopathyDataset(Dataset):
 
         self.transform = transform
         self.max_images = max_images
-        self.balanced_subset_per_class = balanced_subset_per_class
 
         csv_path = os.path.join(dataset_dir, "train.csv")
         images_dir = os.path.join(dataset_dir, split)
