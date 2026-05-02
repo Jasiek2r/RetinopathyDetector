eaa58a87a37c158730d9dde0f28274f39f1edf3b
Jasiek2r <jasiek2r@gmail.com>
2026-05-02 20:43:27 +0200

    feat: new pre-augmented dataset

--- ml/concrete/retinopathy_pipeline.py
+++ ml/concrete/retinopathy_pipeline.py
@@ -1,8 +1,7 @@
 from sklearn.model_selection import train_test_split
 from torchvision import transforms
 
-from ml.concrete.retinopathy_dataset import RetinopathyDataset
-
+from ml.concrete.retinopathy_folder_dataset import RetinopathyFolderDataset
 
 class RetinopathyPipeline:
 
@@ -10,40 +9,11 @@ class RetinopathyPipeline:
         df = dataset.df
 
         # --------------------
-        # SPLIT (tylko indeksy/DF)
-        # --------------------
-        train_df, temp_df = train_test_split(
-            df,
-            test_size=0.3,
-            stratify=df["diagnosis"],
-            random_state=42
-        )
-
-        val_df, test_df = train_test_split(
-            temp_df,
-            test_size=0.5,
-            stratify=temp_df["diagnosis"],
-            random_state=42
-        )
-
-        # --------------------
         # TRANSFORMY
         # --------------------
-        train_tf = transforms.Compose([
-            transforms.Resize((384, 384)),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomRotation(15),
-            transforms.RandomResizedCrop(384, scale=(0.9, 1.0)),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )
-        ])
 
-        eval_tf = transforms.Compose([
+        tf = transforms.Compose([
             transforms.Resize((384, 384)),
-            transforms.CenterCrop(384),
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406],
@@ -54,8 +24,8 @@ class RetinopathyPipeline:
         # --------------------
         # DATASETS (3 sztuki)
         # --------------------
-        train_ds = RetinopathyDataset.from_df(train_df, dir_path, train_tf)
-        val_ds = RetinopathyDataset.from_df(val_df, dir_path, eval_tf)
-        test_ds = RetinopathyDataset.from_df(test_df, dir_path, eval_tf)
+        train_ds = RetinopathyFolderDataset(dir_path, split="train", transform=tf)
+        val_ds = RetinopathyFolderDataset(dir_path, split="val", transform=tf)
+        test_ds = RetinopathyFolderDataset(dir_path, split="test", transform=tf)
 
         return train_ds, val_ds, test_ds
