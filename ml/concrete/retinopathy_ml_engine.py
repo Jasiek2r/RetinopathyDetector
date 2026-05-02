fb3cf8856e359dcb729d1b23405012a04cc55586
Jasiek2r <jasiek2r@gmail.com>
2026-05-02 20:59:26 +0200

    feat: use convnext_base

--- ml/concrete/retinopathy_ml_engine.py
+++ ml/concrete/retinopathy_ml_engine.py
@@ -147,7 +147,7 @@ class RetinopathyMLEngine(MLEngine):
 
     def create_model(self, num_classes=5):
         model = timm.create_model(
-            "convnext_tiny",
+            "convnext_base",
             pretrained=True,
             num_classes=num_classes
         )
