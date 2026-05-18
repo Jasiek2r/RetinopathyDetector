5a10c8b52964e94c3aa297ef7c743a7a2f80a636
Jasiek2r <jasiek2r@gmail.com>
2026-05-18 21:04:47 +0200

    chore: change cohen metric to /100 for better plotting

--- ml/concrete/QWK.py
+++ ml/concrete/QWK.py
@@ -5,4 +5,4 @@ from ml.abstractions.metric import Metric
 
 class QWK(Metric):
     def perform_measurement(self, y_true, y_pred):
-        return 100.0 * cohen_kappa_score(y_true, y_pred, weights="quadratic")
+        return cohen_kappa_score(y_true, y_pred, weights="quadratic")
