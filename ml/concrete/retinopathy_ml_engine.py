18238fa3894711b71db5441a9b61a53ceabcca95
Jasiek2r <jasiek2r@gmail.com>
2026-05-18 21:24:34 +0200

    feat: SimpleCNN

--- ml/concrete/retinopathy_ml_engine.py
+++ ml/concrete/retinopathy_ml_engine.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 class RetinopathyMLEngine(MLEngine):
     def __init__(self, device=None):
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = self.create_model()
+        #self.model = self.create_model()
+        self.model = SimpleCNN()
 
     # ---------------- TRAIN ----------------
     def train(self, train_dataset, val_dataset=None, batch_size=4, epochs=50):
