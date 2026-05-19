a9ff3fb58aeb1178bf12253115d3a907bce714df
Jasiek2r <jasiek2r@gmail.com>
2026-05-19 09:20:02 +0200

    chore: change to split-1

--- startup.py
+++ startup.py
@@ -13,7 +13,7 @@ class Startup:
     def __build_application__(self) -> RetinopathyApplication:
         # configure constants here
         sample_limit = None  # use None for no limit
-        path = "./split-2"
+        path = "./split-1"
         dev = True  # uses developer version for testing
 
         # configure the dependencies used by application here
