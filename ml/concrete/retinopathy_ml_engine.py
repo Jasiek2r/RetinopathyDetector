abcec172c14d3bf7aa27ce51e98511044c93e001
Jasiek2r <jasiek2r@gmail.com>
2026-05-28 16:49:30 +0200

    feat: try to unlock only last blocks

--- ml/concrete/retinopathy_ml_engine.py
+++ ml/concrete/retinopathy_ml_engine.py
@@ -235,6 +235,9 @@ class RetinopathyMLEngine(MLEngine):
         # freeze backbone
         for p in backbone.parameters():
             p.requires_grad = False
+        for name, param in backbone.named_parameters():
+            if "blocks.11" in name or "blocks.10" in name:
+                param.requires_grad = True
 
         hidden = backbone.config.hidden_size
 
