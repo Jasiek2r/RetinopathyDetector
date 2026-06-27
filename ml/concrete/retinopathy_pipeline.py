039f6f432453bff67d5feddd89ab153ac1691246
Jasiek2r <jasiek2r@gmail.com>
2026-06-27 13:40:57 +0200

    feat: more agressive augumentation

--- ml/concrete/retinopathy_pipeline.py
+++ ml/concrete/retinopathy_pipeline.py
@@ -11,9 +11,21 @@ class RetinopathyPipeline(DataPipeline):
         tf = T.Compose([
             T.ToImage(),
             T.ToDtype(torch.float32, scale=True),
-            T.RandomRotation(degrees=5),
+            T.RandomResizedCrop(
+                size=(224, 224),
+                scale=(0.80, 1.00),
+                ratio=(0.9, 1.1)
+            ),
+            T.ColorJitter(
+                brightness=0.2,
+                contrast=0.2,
+                saturation=0.2,
+                hue=0.02
+            ),
             T.RandomHorizontalFlip(p=0.5),
             T.RandomVerticalFlip(p=0.1),
+            T.GaussianBlur(kernel_size=3, sigma=(0.1, 1.0)),
+            T.RandomRotation(degrees=5),
             T.RandomAffine(
                 degrees=5,
                 translate=(0.02, 0.02),  # 2% przesunięcia
