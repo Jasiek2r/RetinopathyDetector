2fa0d9a15bb2cbb048d72344ab5ab096c56a6a12
Jasiek2r <jasiek2r@gmail.com>
2026-06-30 13:41:17 +0200

    chore: effnetb0

--- ml/concrete/retinopathy_ml_engine.py
+++ ml/concrete/retinopathy_ml_engine.py
@@ -20,8 +20,8 @@ class RetinopathyMLEngine(MLEngine):
 
     def __init__(self, provider: ModelProvider, device=None):
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
-        #self.model = provider.create_conv_model().to(self.device)
-        self.model = provider.create_model().to(self.device)
+        self.model = provider.create_conv_model().to(self.device)
+        #self.model = provider.create_model().to(self.device)
         #self.model = provider.create_retfound().to(self.device)
 
         # precompute normalization (IMPORTANT)
